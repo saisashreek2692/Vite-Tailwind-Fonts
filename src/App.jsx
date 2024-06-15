037e6cb0ffee08168f9e013ea2ee2b3ae4dc55a0
Sai Sashreek D <saisashreekd@gmail.com>
2024-06-15 15:43:07 +0530

    Project Created

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl text-rose-500 font-medium">
+        Welcome to vite React
+      </h1>
+      <p className="p-2 text-justify font-normal text-teal-500 font-Poppins">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo
+        molestias voluptate odit labore culpa, hic vero nulla molestiae sunt et
+        odio voluptas porro debitis aliquid rerum sed reiciendis fuga eius?
+      </p>
+    </>
+  );
+}
+
+export default App;
